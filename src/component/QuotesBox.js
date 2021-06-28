d8c261393c47476f9f457f6f25f03e365a316b16
cervantes2907 <uvaldocervantes57>
2021-06-28 18:58:16 -0500

    adding ref link

--- src/component/QuotesBox.js
+++ src/component/QuotesBox.js
@@ -12,7 +12,7 @@ export default function Quotes(props) {
       {props.quote}{/**aqui recibimos por props el array  de citas  */}
       <strong>{props.author}</strong>
       {/*en esta etiqueta <a>creamos el boton de twitter y lo enlazamos a la misma red social en una ventana en blanco */}
-      <a href="https://twitter.com/UbaldoCervante7" target="_blank"><img src="./icon-twitter.svg"/></a>
+      <a href="https://twitter.com/UbaldoCervante7" rel="twitter" target="_blank"><img src="./icon-twitter.svg"/></a>
       </p>
   </div>
 
